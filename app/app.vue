3b3b7189eefb248ca6432730fb4911a42de0ae91
stephaneheyd <heyd.stephane@gmail.com>
2026-01-03 17:58:55 +0100

    Waiting page for future site OK

--- app/app.vue
+++ app/app.vue
@@ -4,13 +4,19 @@ const { data: home } = await useAsyncData(() => queryCollection("content").path(
 </script>
 
 <template>
-  <div class="relative h-screen w-full overflow-hidden flex">
-    <NuxtImg src="background.png" class="absolute inset-0 w-full h-full object-cover -z-10" format="webp" />
-    <div class="h-screen md:w-3/6 lg:w-[340px] p-[26px] bg-linear-to-b from-neutral-500 to-transparent">
-      <h1 class="font-questrial text-[72px] leading-[1.2] text-primary uppercase">{{ title }}</h1>
+  <div class="relative h-screen w-full overflow-hidden flex flex-col lg:flex-row justify-between">
+    <NuxtImg src="background.png" class="absolute inset-0 w-full h-full object-cover object-top -z-10" format="webp" />
+    <div
+      class="h-[250px] md::h-screen w-full lg:w-3/6 lg:w-[340px] p-[26px] bg-linear-to-b from-neutral-500 to-transparent"
+    >
+      <h1 class="font-questrial text-[64px] md:text-[96px] leading-[1.2] text-primary uppercase">{{ title }}</h1>
     </div>
     <div class="flex items-center justify-center">
-      <ContentRenderer v-if="home" :value="home" class="w-1/3 font-questrial" />
+      <ContentRenderer
+        v-if="home"
+        :value="home"
+        class="md:w-full lg:w-2/3 xl:w-1/3 font-questrial text-white text-xl p-[26px] [&_strong]:text-2xl bg-neutral-500/50 md:bg-neutral-500/25 border-4 border-primary"
+      />
     </div>
   </div>
 </template>
--- content/index.md
+++ content/index.md
@@ -1,6 +1,8 @@
 Votre activité mérite d’être mise en avant.
 \
+\
 C’est pour cela que je reviens bientôt afin de vous proposer davantage de services adaptés à développer votre activité en ligne.
 Un site vitrine ou bien un site e-commerce ? Je serais présent pour vous écouter et propulser votre activité sur la toile.
 \
-Puisque votre site est le palier vers votre réussite
+\
+**Puisque votre site est le palier vers votre réussite !**
